6b3c6e83e3aa3a512b438d6502eff6b4cca17374
Kaelin Salazar <kaelinpsalazar@gmail.com>
2022-09-07 11:15:07 -0600

    a few changes with my index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
-const inquirer = require(inquirer);
+const inquirer = require("inquirer");
+const createReadME = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 inquirer
     .prompt([
@@ -8,70 +9,69 @@ inquirer
             type:'input',
             message:'What is the title of the project?',
             name:'title',
-
-
+        },
+        {git
             type:'input',
             message: 'Describe what the project does?',
             name: 'Description',
+        },
+        {
 
             type:'input',
             message:'installation Instructions:',
             name:'installation',
-
+        },
+        {
             type: 'input',
             message:'How does one use it?',
             name: 'usage',
-
+        },
+        {
             type: 'list',
             message: 'Which liscenses do you need?',
             choices: [
-                "Apache 2.0",
-                "GNU GPL v3",
+                "Apache_2.0",
+                "GPLv3",
                 "MIT",
                 "ISC",
                 "None of the above",
             ],
-
-
+            name: 'liscense',
+        },
+        {
             type:'input',
             message:'How you can contribute:',
             name: 'contributers',
-            
+        },
+        {            
             type:'input',
             message:'Test Instructions:',
             name: 'testing',
-
+        },
+        {
             type:'input',
             message:'Github Username:',
             name: 'github',
-
+        },
+        {
             type:'input',
             message:'Email Address:',
             name: 'email',
 
-        }
+        },
     ])
-// // What was your motivation?
-// Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-// What problem does it solve?
-// What did you learn?
-// What makes your project stand out?
+
 
 
 // activity 10
 // TODO: Create a function to write README file
+// inquirer.prompt()
 .then((data) => {
-    let userData = JSON.stringify(data)
 
-    fs.writeFile("./readme-output/README.md", userData, (error) => {
+
+
+    fs.writeFile("./newREADME.md", createReadME(data), (error) => {
         error ? console.error(error) : console.log("Success!");
     });
 });
 
-// File system 
-// TODO: Create a function to initialize app
-function init() {}
-
-
-// Function call to initialize app
-init();
